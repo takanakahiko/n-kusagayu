ab96556c429c26fe879b4780e77782b916f33af3
takanakahiko <takanakahiko@gmail.com>
2021-01-07 21:42:05 +0900

    なんか色々変えた

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,23 @@
 import { number2kanji } from '@geolonia/japanese-numeral'
 import { shareTwitterURL } from 'share-twitter'
 
-function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
+function getRandomInt() {
+    let max = 1
+    for(var i =0; i < 20; i++){
+        if(Math.random() < 0.15){
+            max *= 10
+        }
+    }
+    return Math.floor(Math.random() * Math.floor(max))
 }
 
 document.getElementById("tweet-button").addEventListener("click", e => {
+    const n = getRandomInt()
+    const kanji = number2kanji(n)
     const url = shareTwitterURL({
-        text: `${number2kanji(getRandomInt(9999999999999999))}草粥`,
-        url: 'https://github.com/takanakahiko/n-kusagayu',
+        text: `${kanji}草粥(${n}草粥)`,
+        hashtags: "#nanakusagacha",
+        url: 'https://takanakahiko.me/n-kusagayu',
     })
-    window.open(url);
+    window.open(url)
 })
-
